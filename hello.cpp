93270dd12205cf1d980800ec3f164afe1ffd6f0f
luke-ho <lukeho1@gmail.com>
2013-11-09 14:16:07 -0800

    Adding some C++ because Luke feels like an upgrade from C

---
+++ hello.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Greeting {
+    protected:
+        string s;
+    public:
+        Greeting(){
+            s = "Hello, ";
+        }
+        virtual ~Greeting() { }
+        virtual void printGreeting() = 0;
+};
+
+class GitHubGreeting : public Greeting {
+    public: 
+        GitHubGreeting() { 
+            s += "GitHub!";
+            printGreeting();
+        }
+        GitHubGreeting(GitHubGreeting & h) { }
+        GitHubGreeting(GitHubGreeting * h) { }
+        virtual ~GitHubGreeting() { }
+        void operator=(GitHubGreeting & h) { }
+        void printGreeting() { 
+            cout << s << endl; 
+        }
+};
+
+int main() {
+    GitHubGreeting h;
+    return 0;
+}
