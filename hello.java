dc0765ddeac94781631847bdf96dd86e3a17ac66
Luke Ho <lukeho1@gmail.com>
2013-11-13 22:01:21 -0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
         // I miss SavitchIn
-        System.out.println("Hello, Github!");
+        System.out.println("Hello, GitHub!");
     }
 }
